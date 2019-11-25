f43032d6d00135cac00c04bfeda450bc529540f4
Luis Galeas <luis.galeasjr@gmail.com>
2019-11-25 14:28:05 -0500

    heroku

--- auth/index.js
+++ auth/index.js
@@ -3,10 +3,10 @@ const User = require('../models/User.js')
 
 // Note this is the supper secret for signing the JWT
 // this should be acquired via .env or a microservice
-const JWT_SECRET  = 'thisismysecretkey'
+const JWT_SECRET = 'thisismysecretkey'
 
 // function for creating tokens
-function signToken(user) {
+const signToken = (user) => {
 	// toObject() returns a basic js object 
 	// comprised of data from db. Delete password before creating jwt
 	const userData = user.toObject()
@@ -15,19 +15,19 @@ function signToken(user) {
 }
 
 // function for verifying tokens
-function verifyToken(req, res, next) {
+const verifyToken = (req, res, next) => {
 	// grab token from either headers, req.body, or query string
 	const token = req.get('token') || req.body.token || req.query.token
 	// if no token present, deny access
-	if(!token) return res.json({success: false, message: "No token provided"})
+	if (!token) return res.json({ success: false, message: "No token provided" })
 	// otherwise, try to verify token
 	jwt.verify(token, JWT_SECRET, (err, decodedData) => {
 		// if problem with token verification, deny access
-		if(err) return res.json({success: false, message: "Invalid token."})
+		if (err) return res.json({ success: false, message: "Invalid token." })
 		// otherwise, search for user by id that was embedded in token
 		User.findById(decodedData._id, (err, user) => {
 			// if no user, deny access
-			if(!user) return res.json({success: false, message: "Invalid token."})
+			if (!user) return res.json({ success: false, message: "Invalid token." })
 			// otherwise, add user to req object
 			req.user = user
 			// go on to process the route:
