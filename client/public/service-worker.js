8cac50bb642731e821b66574843c7c3e86aab373
Luis Galeas <luis.galeasjr@gmail.com>
2019-12-02 09:20:05 -0500

    icons #4

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -3,13 +3,7 @@ const FILES_TO_CACHE = [
 	"/assets/css/style.css",
 	"/assets/loadImages.js",
 	"/assets/images/icons/icon-72x72.png",
-	"/assets/images/icons/icon-96x96.png",
-	"/assets/images/icons/icon-128x128.png",
-	"/assets/images/icons/icon-144x144.png",
-	"/assets/images/icons/icon-152x152.png",
 	"/assets/images/icons/icon-192x192.png",
-	"/assets/images/icons/icon-384x384.png",
-	"/assets/images/icons/icon-512x512.png",
 	"/assets/images/Horizon-1-icon.png"
 ];
 
