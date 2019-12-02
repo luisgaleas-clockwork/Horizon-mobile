577489654f48fb56865829a8db05e53314108835
Luis Galeas <luis.galeasjr@gmail.com>
2019-12-02 09:13:50 -0500

    updated icons

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,8 +1,18 @@
 const FILES_TO_CACHE = [
 	"/",
 	"/assets/css/style.css",
-	"/assets/images/icons8-sunrise-64.png"
+	"/assets/loadImages.js",
+	"/assets/images/icons/icon-72x72.png",
+	"/assets/images/icons/icon-96x96.png",
+	"/assets/images/icons/icon-128x128.png",
+	"/assets/images/icons/icon-144x144.png",
+	"/assets/images/icons/icon-152x152.png",
+	"/assets/images/icons/icon-192x192.png",
+	"/assets/images/icons/icon-384x384.png",
+	"/assets/images/icons/icon-512x512.png",
+	"/assets/images/Horizon-1-icon.png"
 ];
+
 const CACHE_NAME = "luis";
 const DATA_CACHE_NAME = "data-luis";
 self.addEventListener("install", function(evt) {
